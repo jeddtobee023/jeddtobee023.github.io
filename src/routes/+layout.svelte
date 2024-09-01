664fdb7674bef12b13038f2b466069db5b1d8046
Jedd Niñonuevo <jed@jasetech.ca>
2024-09-02 00:11:09 +0800

    Test Route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,4 @@
 	<link href="https://fonts.googleapis.com/css2?family=Radio+Canada+Big:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
 </svelte:head>
 
-<main class="min-h-screen min-w-screen">
-	<slot></slot>
-</main>
+<slot></slot>
